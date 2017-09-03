fb598deaa0275e867c34b7e6fffee83d0ba029a4
Bertrand Rigaldies <brigaldies@opensourceconnections.com>
2017-09-02 21:53:09 -0400

    Generate JUnit-compatible XML tests report.

--- src/test/py/runTests.py
+++ src/test/py/runTests.py
@@ -1,9 +1,12 @@
 import os
 import unittest
 
+import xmlrunner
+
 if __name__ == '__main__':
     print("Current directory        : {}".format(os.getcwd()))
     print("os.path.dirname(__file__): {}".format(os.path.dirname(__file__)))
+
     # To run a specific module
     # unittest.main(module='test1')
 
@@ -12,4 +15,12 @@ if __name__ == '__main__':
     # ATTENTION: A __init__.py is necessary in each directory and sub-directory in the search tree for all
     # test files to be discovered.
     suite = unittest.TestLoader().discover('.', pattern='test*.py')
-    unittest.TextTestRunner().run(suite)
+    # unittest.TextTestRunner(verbosity=2).run(suite)
+
+    # Run the tests, and generate the JUnit-compatible XML tests report that Jenkins can consume.
+    with open('unittest-results.xml', 'wb') as output:
+        # unittest.main(
+        #     testRunner=xmlrunner.XMLTestRunner(output=output),
+        #     failfast=False, buffer=False, catchbreak=False)
+
+        xmlrunner.XMLTestRunner(output=output, verbosity=2, failfast=False, buffer=False).run(suite)
